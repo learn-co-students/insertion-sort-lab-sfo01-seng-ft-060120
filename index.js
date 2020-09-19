860f33ba4a7455e9d6e50eb70f31de47a89f62df
iona-b <ionabrabender@gmail.com>
2020-09-19 12:41:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 function findMinAndRemove(array){
 
+    let minNum = array[0];
+    let minIndex = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < minNum) {
+            minNum = array[i]
+            minIndex = i
+        }
+    }
+
+    array.splice(minIndex, 1)
+    return minNum
+
 }
 
 function selectionSort(array){
+
+    let sortedArray = [];
+
+    while (array.length > 0) {
+        sortedArray.push(findMinAndRemove(array))
+    }
+
+    return sortedArray
 
 }
