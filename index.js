70702e80b0109dc976a2bde23653c6f355d5fd19
haleighdalke <haleighdalke@gmail.com>
2020-09-28 17:02:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+    let min = array[0]
+    let minIdx = 0
+    for(let i = 0; i < array.length; i++){
+        if(min > array[i]){
+            min = array[i]
+            minIdx = i
+        }
+    }
+    array.splice(minIdx, 1)
+    return min
 }
 
 function selectionSort(array){
+    let sortedArr = []
+    while(array.length > 0){
+        sortedArr.push(findMinAndRemove(array))
+    }
 
+    return sortedArr
 }
