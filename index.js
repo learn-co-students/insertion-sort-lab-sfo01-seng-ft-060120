d0326cf42b0cd4a8342bc9907f1148d31ca54886
darrickpang <dkypang@yahoo.com>
2020-12-30 17:05:52 -0800

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(arr){
+    let min = arr[0]
+    let index = 0
+    for(var x = 0; x < arr.length; x++){
+        if(arr[x] < min){
+            min = arr[x]
+            index = x
+        }
+    }
+    arr.splice(index, 1)
+    return min
 }
 
-function selectionSort(array){
-
+function selectionSort(arr){
+    let min 
+    let newarr = []
+    while(arr.length !== 0){
+        min = findMinAndRemove(arr)
+        newarr.push(min)
+    }
+    return newarr
 }
